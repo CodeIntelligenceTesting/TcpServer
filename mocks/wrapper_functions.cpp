04e940601932f58c099b45b8bc2bd74c516bc0a4
psy <patrice.salathe@code-intelligence.com>
2023-01-23 10:44:06 +0100

    Update wrapper_functions.cpp

--- mocks/wrapper_functions.cpp
+++ mocks/wrapper_functions.cpp
@@ -2,6 +2,7 @@
 #include "../include/tcp_client.h"
 #include "../include/common.h"
 
+// $ nm build/tcp_server | grep TcpServer.*start
 extern "C" pipe_ret_t __wrap__ZN9TcpServer5startEiib(int port, int maxNumOfClients, bool removeDeadClientsAutomatically) {
     std::cout << std::endl << "Function 'TcpServer::start' wrapped" << std::endl << std::endl;
     return pipe_ret_t::success();
