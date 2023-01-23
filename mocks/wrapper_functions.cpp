1e5006eed3b073a28c178aa4997487de0ec2889a
psy <patrice.salathe@code-intelligence.com>
2023-01-23 10:40:26 +0100

    Add wrapper function example

---
+++ mocks/wrapper_functions.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/tcp_client.h"
+#include "../include/common.h"
+
+extern "C" pipe_ret_t __wrap__ZN9TcpServer5startEiib(int port, int maxNumOfClients, bool removeDeadClientsAutomatically) {
+    std::cout << std::endl << "Function 'TcpServer::start' wrapped" << std::endl << std::endl;
+    return pipe_ret_t::success();
+}
